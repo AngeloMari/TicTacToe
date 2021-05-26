0a8a526eae8b61616be16b12ce0a9db64a457791
AngeloMari <santosangelomari@gmail.com>
2021-05-26 19:34:55 +0800

    About and Help buttons

--- TicTacToe/Form1.Designer.cs
+++ TicTacToe/Form1.Designer.cs
@@ -29,9 +29,9 @@ namespace TicTacToe
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnSTART = new System.Windows.Forms.Button();
-            this.bntRESTART = new System.Windows.Forms.Button();
-            this.btnUNDO = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.bntRestart = new System.Windows.Forms.Button();
+            this.btnUndo = new System.Windows.Forms.Button();
             this.btnNorthWest = new System.Windows.Forms.Button();
             this.btnWest = new System.Windows.Forms.Button();
             this.btnSouthWest = new System.Windows.Forms.Button();
@@ -41,38 +41,40 @@ namespace TicTacToe
             this.btnNorthEast = new System.Windows.Forms.Button();
             this.btnEast = new System.Windows.Forms.Button();
             this.btnSouthEast = new System.Windows.Forms.Button();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.btnAbout = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // btnSTART
+            // btnStart
             // 
-            this.btnSTART.Location = new System.Drawing.Point(-1, -1);
-            this.btnSTART.Name = "btnSTART";
-            this.btnSTART.Size = new System.Drawing.Size(62, 23);
-            this.btnSTART.TabIndex = 0;
-            this.btnSTART.Text = "Start";
-            this.btnSTART.UseVisualStyleBackColor = true;
+            this.btnStart.Location = new System.Drawing.Point(-1, -1);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(62, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
             // 
-            // bntRESTART
+            // bntRestart
             // 
-            this.bntRESTART.Location = new System.Drawing.Point(57, -1);
-            this.bntRESTART.Name = "bntRESTART";
-            this.bntRESTART.Size = new System.Drawing.Size(62, 23);
-            this.bntRESTART.TabIndex = 1;
-            this.bntRESTART.Text = "Restart";
-            this.bntRESTART.UseVisualStyleBackColor = true;
+            this.bntRestart.Location = new System.Drawing.Point(57, -1);
+            this.bntRestart.Name = "bntRestart";
+            this.bntRestart.Size = new System.Drawing.Size(62, 23);
+            this.bntRestart.TabIndex = 1;
+            this.bntRestart.Text = "Restart";
+            this.bntRestart.UseVisualStyleBackColor = true;
             // 
-            // btnUNDO
+            // btnUndo
             // 
-            this.btnUNDO.Location = new System.Drawing.Point(116, -1);
-            this.btnUNDO.Name = "btnUNDO";
-            this.btnUNDO.Size = new System.Drawing.Size(62, 23);
-            this.btnUNDO.TabIndex = 2;
-            this.btnUNDO.Text = "Undo";
-            this.btnUNDO.UseVisualStyleBackColor = true;
+            this.btnUndo.Location = new System.Drawing.Point(116, -1);
+            this.btnUndo.Name = "btnUndo";
+            this.btnUndo.Size = new System.Drawing.Size(62, 23);
+            this.btnUndo.TabIndex = 2;
+            this.btnUndo.Text = "Undo";
+            this.btnUndo.UseVisualStyleBackColor = true;
             // 
             // btnNorthWest
             // 
-            this.btnNorthWest.Location = new System.Drawing.Point(36, 47);
+            this.btnNorthWest.Location = new System.Drawing.Point(45, 47);
             this.btnNorthWest.Name = "btnNorthWest";
             this.btnNorthWest.Size = new System.Drawing.Size(83, 68);
             this.btnNorthWest.TabIndex = 3;
@@ -80,7 +82,7 @@ namespace TicTacToe
             // 
             // btnWest
             // 
-            this.btnWest.Location = new System.Drawing.Point(36, 121);
+            this.btnWest.Location = new System.Drawing.Point(45, 111);
             this.btnWest.Name = "btnWest";
             this.btnWest.Size = new System.Drawing.Size(83, 68);
             this.btnWest.TabIndex = 4;
@@ -88,7 +90,7 @@ namespace TicTacToe
             // 
             // btnSouthWest
             // 
-            this.btnSouthWest.Location = new System.Drawing.Point(36, 195);
+            this.btnSouthWest.Location = new System.Drawing.Point(45, 175);
             this.btnSouthWest.Name = "btnSouthWest";
             this.btnSouthWest.Size = new System.Drawing.Size(83, 68);
             this.btnSouthWest.TabIndex = 5;
@@ -104,7 +106,7 @@ namespace TicTacToe
             // 
             // btnCenter
             // 
-            this.btnCenter.Location = new System.Drawing.Point(125, 121);
+            this.btnCenter.Location = new System.Drawing.Point(125, 111);
             this.btnCenter.Name = "btnCenter";
             this.btnCenter.Size = new System.Drawing.Size(83, 68);
             this.btnCenter.TabIndex = 7;
@@ -112,7 +114,7 @@ namespace TicTacToe
             // 
             // btnSouth
             // 
-            this.btnSouth.Location = new System.Drawing.Point(125, 195);
+            this.btnSouth.Location = new System.Drawing.Point(125, 175);
             this.btnSouth.Name = "btnSouth";
             this.btnSouth.Size = new System.Drawing.Size(83, 68);
             this.btnSouth.TabIndex = 8;
@@ -120,7 +122,7 @@ namespace TicTacToe
             // 
             // btnNorthEast
             // 
-            this.btnNorthEast.Location = new System.Drawing.Point(214, 47);
+            this.btnNorthEast.Location = new System.Drawing.Point(203, 47);
             this.btnNorthEast.Name = "btnNorthEast";
             this.btnNorthEast.Size = new System.Drawing.Size(83, 68);
             this.btnNorthEast.TabIndex = 9;
@@ -129,7 +131,7 @@ namespace TicTacToe
             // 
             // btnEast
             // 
-            this.btnEast.Location = new System.Drawing.Point(214, 121);
+            this.btnEast.Location = new System.Drawing.Point(203, 111);
             this.btnEast.Name = "btnEast";
             this.btnEast.Size = new System.Drawing.Size(83, 68);
             this.btnEast.TabIndex = 10;
@@ -137,17 +139,39 @@ namespace TicTacToe
             // 
             // btnSouthEast
             // 
-            this.btnSouthEast.Location = new System.Drawing.Point(214, 195);
+            this.btnSouthEast.Location = new System.Drawing.Point(203, 175);
             this.btnSouthEast.Name = "btnSouthEast";
             this.btnSouthEast.Size = new System.Drawing.Size(83, 68);
             this.btnSouthEast.TabIndex = 11;
             this.btnSouthEast.UseVisualStyleBackColor = true;
             // 
+            // btnHelp
+            // 
+            this.btnHelp.Location = new System.Drawing.Point(331, -1);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(62, 23);
+            this.btnHelp.TabIndex = 12;
+            this.btnHelp.Text = "Help";
+            this.btnHelp.UseVisualStyleBackColor = true;
+            this.btnHelp.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.Location = new System.Drawing.Point(272, -1);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(62, 23);
+            this.btnAbout.TabIndex = 13;
+            this.btnAbout.Text = "About";
+            this.btnAbout.UseVisualStyleBackColor = true;
+            this.btnAbout.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(326, 287);
+            this.ClientSize = new System.Drawing.Size(393, 323);
+            this.Controls.Add(this.btnAbout);
+            this.Controls.Add(this.btnHelp);
             this.Controls.Add(this.btnSouthEast);
             this.Controls.Add(this.btnEast);
             this.Controls.Add(this.btnNorthEast);
@@ -157,9 +181,9 @@ namespace TicTacToe
             this.Controls.Add(this.btnSouthWest);
             this.Controls.Add(this.btnWest);
             this.Controls.Add(this.btnNorthWest);
-            this.Controls.Add(this.btnUNDO);
-            this.Controls.Add(this.bntRESTART);
-            this.Controls.Add(this.btnSTART);
+            this.Controls.Add(this.btnUndo);
+            this.Controls.Add(this.bntRestart);
+            this.Controls.Add(this.btnStart);
             this.Name = "Form1";
             this.Text = "Tic Tac Toe";
             this.ResumeLayout(false);
@@ -168,9 +192,9 @@ namespace TicTacToe
 
         #endregion
 
-        private System.Windows.Forms.Button btnSTART;
-        private System.Windows.Forms.Button bntRESTART;
-        private System.Windows.Forms.Button btnUNDO;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button bntRestart;
+        private System.Windows.Forms.Button btnUndo;
         private System.Windows.Forms.Button btnNorthWest;
         private System.Windows.Forms.Button btnWest;
         private System.Windows.Forms.Button btnSouthWest;
@@ -180,6 +204,8 @@ namespace TicTacToe
         private System.Windows.Forms.Button btnNorthEast;
         private System.Windows.Forms.Button btnEast;
         private System.Windows.Forms.Button btnSouthEast;
+        private System.Windows.Forms.Button btnHelp;
+        private System.Windows.Forms.Button btnAbout;
     }
 }
 
--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -21,5 +21,28 @@ namespace TicTacToe
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("How to play Tic Tac Toe" +
+                "\n\n\n" + "1. The game should be played on a grid of 3 by 3 squares." +
+                "\n\n2. It is played by 2 players. One player will use 'X,' while the other is 'O,'" +
+                " to mark the square alternately." +
+                "\n\n3. The first player to mark the squares 3 in a row, up, down, or diagonal, will be the winner." +
+                "\n\n4. When all the squares are full, the game is over." +
+                "\n\n5. If the squares are full, but there are no patterns created, then the game is draw.");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Tic Tac Toe\n\nIt is one of the most known games in the world. " +
+                "The game was even found out inside the temples of Egypt, where ancient Egyptians believe that it is a 'Magic Square.' " +
+                "Experts believe that the name Tic Tac Toe is from a 19th-century popular game " +
+                "called 'ticktack,' and it has different rules. " + "On the other hand, it said that the original name" +
+                " of the game in the 16th-century is called 'Tit-tat-toe.'" + "\n\n\n" +
+                "If you want to read more, check out:\n" +
+                "\nhttp://gamescrafters.berkeley.edu/games.php?game=tictactoe" +
+                "\nhttps://wonderopolis.org/wonder/how-old-is-tic-tac-toe");
+        }
     }
 }
